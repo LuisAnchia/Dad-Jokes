47759558967e51a9cf830da0f966753a93a7a10f
LuisAnchia <108684856+LuisAnchia@users.noreply.github.com>
2023-05-13 01:31:21 -0600

    ajua
    i created the base of first workhome, but a i need complete many things more, for example the search and export and import

---
+++ Dad jocker/chistesRandom.js
@@ -0,0 +1,14 @@
+
+
+const randomJoker = "https://icanhazdadjoke.com/";
+
+document.getElementById("buttonForJoker").addEventListener("click", Jokerin);
+
+async function Jokerin(){
+    await fetch(randomJoker, { 
+        method: "GET",
+        headers: {'Accept': 'application/json'} })
+    .then((resp) => resp.json())
+    .then(data => document.getElementById("joker").innerHTML = data.joke);  
+}
+
