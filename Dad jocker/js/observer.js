487b4f1ef94be5eb0be1c386dbaa950ce74f9879
LuisAnchia <108684856+LuisAnchia@users.noreply.github.com>
2023-05-30 22:59:28 -0600

    Titule
    i put the new observer for titule

--- Dad jocker/js/observer.js
+++ Dad jocker/js/observer.js
@@ -11,20 +11,43 @@ export class ImageObserver {
 
     if (image.includes('product-case-white')) {
       price = 5;
+      const title = document.querySelector('h1');
+      title.textContent = "Product Case white";
+
     } else if (image.includes('product-case-black')) {
       price = 7;
+      const title = document.querySelector('h1');
+      title.textContent = "Product Case Black";
+
     } else if (image.includes('product-poster-white')) {
       price = 3;
+      const title = document.querySelector('h1');
+      title.textContent = "Product poster white";
+
     } else if (image.includes('product-poster-black')) {
       price = 5;
+      const title = document.querySelector('h1');
+      title.textContent = "Product poster black";
+
     } else if (image.includes('product-shirt-white')) {
       price = 10;
+      const title = document.querySelector('h1');
+      title.textContent = "Product shirt white";
+
     } else if (image.includes('product-shirt-black')) {
       price = 13;
+      const title = document.querySelector('h1');
+      title.textContent = "Product shirt black";
+
     } else if (image.includes('product-pillow-white')) {
       price = 12;
+      const title = document.querySelector('h1');
+      title.textContent = "Product pillow white";
+
     } else if (image.includes('product-pillow-black')) {
       price = 15;
+      const title = document.querySelector('h1');
+      title.textContent = "Product pillow black";
     }
 
     priceElement.textContent = '$' + price;
