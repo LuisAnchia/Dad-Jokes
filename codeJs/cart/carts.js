daa8e29bf4a74b81c70c4b49e17ecf4cefd75eae
LuisAnchia <108684856+LuisAnchia@users.noreply.github.com>
2023-06-06 00:40:06 -0600

    up
    up

--- codeJs/cart/carts.js
+++ codeJs/cart/carts.js
@@ -13,43 +13,57 @@ export function addEventListeners() {
     agregarBtns[i].addEventListener('click', function() {
       let imagenPrincipal = document.getElementById('image');
       let imagen = imagenPrincipal ? imagenPrincipal.getAttribute('src') : this.getAttribute('data-imagen');
-      let producto = new Producto(this.getAttribute('data-producto'), imagen, this.getAttribute('data-precio'));
+      let precio = imagenPrincipal && imagenPrincipal.getAttribute('src').includes('product-shirt-white') ? '10'
+                 : imagenPrincipal && imagenPrincipal.getAttribute('src').includes('product-shirt-black') ? '13' 
+                 : imagenPrincipal && imagenPrincipal.getAttribute('src').includes('product-case-black') ? '7' 
+                 : imagenPrincipal && imagenPrincipal.getAttribute('src').includes('product-case-white') ? '5'
+                 : imagenPrincipal && imagenPrincipal.getAttribute('src').includes('product-pillow-white') ? '12'
+                 : imagenPrincipal && imagenPrincipal.getAttribute('src').includes('product-pillow-black') ? '15'
+                 : imagenPrincipal && imagenPrincipal.getAttribute('src').includes('product-poster-white') ? '3'
+                 : imagenPrincipal && imagenPrincipal.getAttribute('src').includes('product-poster-black') ? '5': this.getAttribute('data-precio');
+      let nombre = imagenPrincipal && imagenPrincipal.getAttribute('src').includes('product-shirt-white') ? 'Product Shirt White'
+                 : imagenPrincipal && imagenPrincipal.getAttribute('src').includes('product-shirt-black') ? 'Product Shirt Black' 
+                 : imagenPrincipal && imagenPrincipal.getAttribute('src').includes('product-case-black') ? 'Product case Black'
+                 : imagenPrincipal && imagenPrincipal.getAttribute('src').includes('product-case-white') ? 'Product case white'
+                 : imagenPrincipal && imagenPrincipal.getAttribute('src').includes('product-pillow-white') ? 'Product pillow white' 
+                 : imagenPrincipal && imagenPrincipal.getAttribute('src').includes('product-pillow-black') ? 'Product pillow black' 
+                 : imagenPrincipal && imagenPrincipal.getAttribute('src').includes('product-poster-white') ? 'Product poster white' 
+                 : imagenPrincipal && imagenPrincipal.getAttribute('src').includes('product-poster-black') ? 'Product poster black' : imagenPrincipal && imagenPrincipal.getAttribute('src').includes('product-poster-black') ? 'Product poster black' :  this.getAttribute('data-producto');
+      let producto = new Producto(nombre, imagen, precio);
       carrito.push(producto);
-      alert('Producto "' + producto.nombre + '" agregado al carrito.');
+      alert('Product "' + producto.nombre + '" Added the Cart.');
     });
   }
 
   const eliminarTodoBtn = document.getElementById("eliminarTodoBtn");
+  eliminarTodoBtn.addEventListener("click", function() {
+    carrito = [];
+    actualizarLista();
+  });
 
-eliminarTodoBtn.addEventListener("click", function() {
-  carrito = [];
-  actualizarLista();
-});
+  const eliminarUnoBtn = document.getElementById("eliminarUnoBtn");
+  eliminarUnoBtn.addEventListener("click", function() {
+    carrito.pop();
+    actualizarLista();
+  });
 
-const eliminarUnoBtn = document.getElementById("eliminarUnoBtn");
-
-eliminarUnoBtn.addEventListener("click", function() {
-  carrito.pop();
-  actualizarLista();
-});
-
-function actualizarLista() {
-  carritoList.innerHTML = '';
-  if (carrito.length === 0) {
-    carritoList.innerHTML = 'No hay productos pendientes.';
-  } else {
-    for (let j = 0; j < carrito.length; j++) {
-      let producto = carrito[j];
-      let li = producto.mostrarProducto();
-      carritoList.appendChild(li);
+  function actualizarLista() {
+    carritoList.innerHTML = '';
+    if (carrito.length === 0) {
+      carritoList.innerHTML = 'Don´t there any products in this moment..';
+    } else {
+      for (let j = 0; j < carrito.length; j++) {
+        let producto = carrito[j];
+        let li = producto.mostrarProducto();
+        carritoList.appendChild(li);
+      }
     }
   }
-}
 
   verCarritoBtn.addEventListener('click', function() {
     carritoList.innerHTML = '';
     if (carrito.length === 0) {
-      carritoList.innerHTML = 'No hay productos pendientes.';
+      carritoList.innerHTML = 'Don´t there any products in this moment.';
     } else {
       for (let j = 0; j < carrito.length; j++) {
         let producto = carrito[j];
@@ -70,4 +84,3 @@ function actualizarLista() {
     }
   });
 }
-
