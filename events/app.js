0f4dbf7ae0b900a1f38d33dc9d8512b9008a4631
LuisAnchia <108684856+LuisAnchia@users.noreply.github.com>
2023-06-11 13:30:00 -0600

    up
    up

--- events/app.js
+++ events/app.js
@@ -1,16 +1,15 @@
-/**import { callJoker } from '../codeJs/random.js';**/
 import { displayJoke } from '../codeJs/api/domRandom.js';
-// Llamar a la función displayJoke cuando se hace clic en un botón
+import { addEventListeners } from '../codeJs/cart/carts.js';
+import { showJoke } from '../codeJs/callRandom.js';
+import { search } from '../codeJs/search.js';
+
+
 const buttonForJoker = document.getElementById("buttonForJoker");
 buttonForJoker.addEventListener("click", displayJoke);
-import { addEventListeners } from '../codeJs/cart/carts.js';
 addEventListeners();
-import { showJoke } from '../codeJs/callRandom.js';
 showJoke();
 const jokeText = localStorage.getItem('jokeAbout');
 if (jokeText) {
 showJokeOverlay(jokeText);s
 localStorage.removeItem('jokeAbout');
 }
-
-import { search } from '../codeJs/search.js';
