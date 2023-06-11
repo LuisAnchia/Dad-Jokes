a2e1e523f9b86f9b715d9f3e3c348a591599d62c
LuisAnchia <108684856+LuisAnchia@users.noreply.github.com>
2023-06-11 15:08:40 -0600

    up
    up

--- events/publisher.js
+++ events/publisher.js
@@ -1,58 +1,20 @@
-/**import { callJoker } from '../codeJs/random.js';**/
 import { displayJoke } from '../codeJs/api/domRandom.js';
-
-// Llamar a la función displayJoke cuando se hace clic en un botón
-const buttonForJoker = document.getElementById("buttonForJoker");
-buttonForJoker.addEventListener("click", displayJoke);
 import { addEventListeners } from '../codeJs/cart/carts.js';
-addEventListeners();
 import { showJoke } from '../codeJs/callRandom.js';
-showJoke();
+import { ImageObserver } from './observer.js';
 
+const imageObserver = new ImageObserver();
+const imageElement = document.getElementById('image');
+showJoke();
 const jokeText = localStorage.getItem('jokeAbout');
 if (jokeText) {
 showJokeOverlay(jokeText);s
 localStorage.removeItem('jokeAbout');
 }
+addEventListeners();
+const buttonForJoker = document.getElementById("buttonForJoker");
+buttonForJoker.addEventListener("click", displayJoke);
 
-import { ImageObserver } from './observer.js';
-const imageObserver = new ImageObserver();
-const imageElement = document.getElementById('image');
-
-const colorMappings = {
-  'product-case': { white: 'black', black: 'white' },
-  'product-poster': { white: 'black', black: 'white' },
-  'product-shirt': { white: 'black', black: 'white' },
-  'product-pillow': { white: 'black', black: 'white' }
-};
-
-const products = {
-  'product-case': {
-    price: { white: 5, black: 7 },
-    title: 'Product Case',
-    colors: ['white', 'black']
-  },
-  'product-poster': {
-    price: { white: 3, black: 5 },
-    title: 'Product Poster',
-    colors: ['white', 'black']
-  },
-  'product-shirt': {
-    price: { white: 10, black: 13 },
-    title: 'Product Shirt',
-    colors: ['white', 'black']
-  },
-  'product-pillow': {
-    price: { white: 12, black: 15 },
-    title: 'Product Pillow',
-    colors: ['white', 'black']
-  }
-};
-
-imageElement.addEventListener('load', () => {
-  const currentImage = imageElement.src;
-  imageObserver.update(currentImage);
-});
 
 const thumbnailImages = document.getElementsByClassName('thumbnail');
 for (const thumbnailImage of thumbnailImages) {
